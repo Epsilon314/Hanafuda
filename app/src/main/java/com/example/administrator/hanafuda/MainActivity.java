bc136f80e83dea6ad2103c94460637d8adb67da4
epsilon <yiqing_zhu2015@126.com>
2018-05-06 22:31:22 +0800

    add two diag

--- app/src/main/java/com/example/administrator/hanafuda/MainActivity.java
+++ app/src/main/java/com/example/administrator/hanafuda/MainActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -49,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         about.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                showAboutDiag();
             }
         });
 
@@ -57,6 +59,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void showHelpDiag() {
         final AlertDialog.Builder help = new AlertDialog.Builder(this);
+        help.setTitle("帮助");
+        TextView tv = new TextView(this);
+        tv.setText(R.string.helpcontent);
+        tv.setMovementMethod(LinkMovementMethod.getInstance());
+        help.setView(tv);
         help.show();
+    }
+
+    public void showAboutDiag() {
+        final AlertDialog.Builder about = new AlertDialog.Builder(this);
+        about.setTitle("关于");
+        about.setMessage("花札\n嵌入式课程设计");
+        about.show();
     }
 }
